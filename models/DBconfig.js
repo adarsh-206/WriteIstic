5d78500fb33ebae14fa5a225b4e43d0054542e54
adarsh8878 <66101827+adarsh8878@users.noreply.github.com>
2021-03-17 23:16:14 +0530

    Update DBconfig.js

--- models/DBconfig.js
+++ models/DBconfig.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 /* Map Global promises */
 mongoose.Promise = global.Promise;
 
-mongoose.connect('mongodb://localhost:27017/Blog_DB', { useNewUrlParser: true }, (err) => {
+mongoose.connect(process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Blog_DB', { useNewUrlParser: true }, (err) => {
     if (!err) {
         console.log('Connection created.')
     }
