3049bc130d412042f4fdfcdf66da72daa8a6e9a8
adarsh8878 <66101827+adarsh8878@users.noreply.github.com>
2021-03-18 16:03:20 +0530

    Update DBconfig.js

--- models/DBconfig.js
+++ models/DBconfig.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const uri = "mongodb+srv://writeistic:<password>@cluster0.7v7bx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
+const url = "mongodb+srv://writeistic:<password>@cluster0.7v7bx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority";
 
 mongoose.connect(url, { useNewUrlParser: true }, (err) => {
     if (!err) {
