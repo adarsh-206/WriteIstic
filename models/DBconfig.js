7ace46ae94dde529ccf6265ce0a0bc58b2b8ecd9
adarsh8878 <66101827+adarsh8878@users.noreply.github.com>
2021-03-18 18:35:53 +0530

    Update DBconfig.js

--- models/DBconfig.js
+++ models/DBconfig.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI || process.env.MONGOHQ_URL || process.env.MONGOLAB_URI || 'mongodb://localhost:27017/Blog_DB', { useNewUrlParser: true }, (err) => {
+mongoose.connect('mongodb+srv://Blog_DB:Ada2001r$h@writeistic.7v7bx.mongodb.net/myFirstDatabase?retryWrites=true&w=majority', { useNewUrlParser: true }, (err) => {
     if (!err) {
         console.log('Connection created.')
     }
